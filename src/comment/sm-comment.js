9f831dfb36ade4f7cf99ca8a28babaf453e803ea
Philip Diffenderfer <pdiffenderfer@gmail.com>
2015-08-15 19:27:28 -0400

    Comments Directive & Functional Example

--- src/comment/sm-comment.js
+++ src/comment/sm-comment.js
@@ -10,6 +10,8 @@
 
       replace: true,
 
+      // transclude: true,
+
       scope: {
         /* Required */
         comments: '=',
@@ -17,37 +19,68 @@
         /* Optional */
         avatar: '&',
         author: '&',
-        authorClick: '&',
         date: '&',
         replies: '&',
         reply: '=',
-        replyClick: '&',
-        replyText: '@'
+        collapsible: '=',
+
+        onAuthor: '&',
+        onReply: '&',
+        onShowReplies: '&',
+        onHideReplies: '&'
       },
 
       template: [
-        '<div class="ui comments" ng-class="{threaded: reply}">',
+        '<div class="ui comments">',
         ' <div class="comment" ng-repeat="c in comments" ng-init="$ = {comment: c}">',
-        '  <a ng-if="avatar($)" class="avatar" ng-click="authorClick($)">',
+        '  <a ng-if="avatar($)" class="avatar" ng-click="onAuthor({comment: c, $event: $event})">',
         '    <img ng-src="{{ avatar($) }}">',
         '  </a>',
         '  <div class="content">',
-        '   <a class="author" ng-click="authorClick($)" sm-html="author($)"></a>',
+        '   <a class="author" ng-click="onAuthor({comment: c, $event: $event})" sm-html="author($)"></a>',
         '   <div class="metadata">',
         '    <span class="date" sm-time-ago="date($)"></span>',
         '   </div>',
         '   <div class="text" sm-html="content($)"></div>',
         '   <div class="actions">',
-        '     <a class="reply" ng-click="replyClick($)" ng-show="reply" sm-html-once="replyText"></a>',
+        '     <a class="reply" ng-click="onReply({comment: c, $event: $event})" ng-if="reply">Reply</a>',
+        '     <a class="show-replies" ng-if="reply && collapsible && isCollapsed" href ng-click="setCollapsed(comment, $event, false)" sm-html="getShowRepliesText($)"></a>',
+        '     <a class="hide-replies" ng-if="reply && collapsible && !isCollapsed" href ng-click="setCollapsed(comment, $event, true)" sm-html="getHideRepliesText($)"></a>',
         '   </div>',
         '  </div>',
-        '  <sm-comments ng-if="hasReplies($)" comments="replies($)" content="content({comment: comment})" avatar="avatar({comment: comment})" author="author({comment: comment})" author-click="authorClick({comment: comment})" date="date({comment: comment})" replies="replies({comment: comment})" reply="reply" reply-click="replyClick({comment: comment})" reply-text="{{ replyText }}"></sm-comments>',
+        '  <sm-comments ng-if="hasReplies($)" ng-class="{collapsed: collapsible && isCollapsed}" comments="replies($)" content="content({comment: comment})" avatar="avatar({comment: comment})" author="author({comment: comment})" date="date({comment: comment})" replies="replies({comment: comment})" reply="reply" collapsible="collapsible"',
+        '     on-author="onAuthor({comment: comment, $event: $event})" on-reply="onReply({comment: comment, $event: $event})" on-show-replies="onShowReplies({comment: comment, $event: $event})" on-hide-replies="onHideReplies({comment: comment, $event: $event})"></sm-comments>',
         ' </div>',
         '</div>'
       ].join('\n'),
 
       controller: function($scope)
       {
+        $scope.isCollapsed = true;
+
+        $scope.setCollapsed = function(comment, $event, collapse)
+        {
+          var $ = {comment: comment, $event: $event};
+
+          if ( $scope.isCollapsed !== collapse )
+          {
+            if ( $scope.isCollapsed )
+            {
+              if ( $scope.onShowReplies($) !== false )
+              {
+                $scope.isCollapsed = false;
+              }
+            }
+            else
+            {
+              if ( $scope.onHideReplies($) !== false )
+              {
+                $scope.isCollapsed = true;
+              }
+            }
+          }
+        };
+
         $scope.hasReplies = function($)
         {
           if ( !$scope.reply )
@@ -58,6 +91,32 @@
           var replies = $scope.replies($);
 
           return replies && replies.length;
+        };
+
+        $scope.getReplyCount = function($)
+        {
+          if ( !$scope.reply )
+          {
+            return false;
+          }
+
+          var replies = $scope.replies($);
+
+          return replies ? replies.length : 0;
+        };
+
+        $scope.getShowRepliesText = function($)
+        {
+          var count = $scope.getReplyCount($);
+
+          return count === 0 ? '' : (count === 1 ? 'Show Reply' : 'Show Replies (' + count + ')');
+        };
+
+        $scope.getHideRepliesText = function($)
+        {
+          var count = $scope.getReplyCount($);
+
+          return count === 0 ? '' : (count === 1 ? 'Hide Reply' : 'Hide Replies (' + count + ')');
         };
       },
 
